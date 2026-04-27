74327c21750aa5694abd3f80b52aeb3bf4a56fc5
Manoj Kumar <manojkumar@bridgeindia.dev>
2026-04-27 10:47:52 +0530

    update: full application code for Bridge India

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Smart-resource-allocation/',
   server: {
     port: 5173,
     open: true,
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
+    sourcemap: false,
   },
 })
